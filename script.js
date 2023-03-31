282eaef5d1e694bd013553a0137f7dc86906c9ef
MicheleBertoia2 <michelebertoia95@gmail.com>
2023-03-31 14:53:23 +0200

    [add] steps

---
+++ script.js
@@ -0,0 +1,15 @@
+// **Descrizione:**
+// Visualizzare in pagina 5 numeri casuali (tutti differenti). Da lì parte un timer di 5 secondi.
+// Dopo 5 secondi i numeri scompaiono e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente(tramite il prompt() o traminte input).
+// Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+/*
+1. genero un numero N di numeri unici e li salvo in un array
+2. li stampo in pagina 
+3. creo una funzione che dopo 5 secondi dal click cancella i numeri
+4. chiedo di inserire i numeri e li salvo in un array
+5. con una funzione verifico che i numeri inseriti corrispondano a quelli generati
+6.stampo i risultati diversi
+*/
+
+
